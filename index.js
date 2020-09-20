27a9e7ab4287ad195c1e5318f68c90eeb6fae10f
Magic302 <47436007+Magic302@users.noreply.github.com>
2020-09-19 21:55:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, Collection } = require('discord.js')
+const fs = require('fs') 
+ 
+const client = new Client(); 
+ 
+client.commands = new Collection(); 
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/"); 
+
+const cooldowns = new Collection();
+ 
+["aliases", "commands"].forEach(x => client[x] = new Collection());
+["command", "event"].forEach(x => require(`./main/${x}`)(client));
+ 
+client.on('ready', () => {
+    console.log('APP - Ligado')
+})
+ 
+client.login("")
